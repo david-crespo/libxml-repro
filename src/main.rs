f73474b29e18a8d41bd458fd6c3737781afb2062
David Crespo <david.crespo@oxidecomputer.com>
2025-01-18 17:35:54 -0600

    take out unnecessary lines

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ const SAML_RESPONSE: &str = include_str!("../base64_saml_response.txt");
 
 fn main() {
     let mut sp_builder = ServiceProviderBuilder::default();
-    sp_builder.acs_url(Some("https://customer.site/oxide_rack/saml".to_string()));
-    sp_builder.slo_url(Some("https://customer.site/oxide_rack/saml".to_string()));
-    sp_builder.authn_name_id_format(Some(NameIdFormat::UnspecifiedNameIDFormat.value().into()));
-
     let idp_metadata: EntityDescriptor = SAML_RESPONSE_IDP_DESCRIPTOR.parse().unwrap();
     sp_builder.idp_metadata(idp_metadata);
 
